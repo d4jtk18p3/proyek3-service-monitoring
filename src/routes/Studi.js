c065cb3b1d60ebddf75587a370f634ddbc73632e
Dwinanda <dwinanda.alfauzan.tif418@polban.ac.id>
2021-07-16 16:11:24 +0700

    Update routes

--- src/routes/Studi.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as StudiController from '../controller/Studi'
-
-const router = express.Router()
-
-router.get('/:id', StudiController.getStudiByID)
-
-export default router
